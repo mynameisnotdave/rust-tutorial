622091f76caacfda4a5b70ac6c1a3aae166576e2
mynameisnotdave <islandmonkeee@gmail.com>
2023-12-22 18:28:30 +0000

    comments

--- src/structs.rs
+++ src/structs.rs
@@ -1,3 +1,4 @@
+// Here we are defining the members of the struct and their types.
 struct User {
     active: bool,
     username: String,
@@ -5,6 +6,7 @@ struct User {
     sign_in_count: u64
 }
 
+// Then, we are assigning values to the struct members.
 fn struct_impl() -> () {
     let user1 = User {
         active: true,
@@ -12,4 +14,14 @@ fn struct_impl() -> () {
         email: String::from("someone@example.com"),
         sign_in_count: 1
     };
+}
+
+// Here, we have a function that returns the User struct.
+fn build_user(email: String, username: String) -> User {
+    return User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1
+    }
 }
