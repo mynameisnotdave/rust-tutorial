7af821f8cc19f2dcff5d5b4671d19a1535aba984
mynameisnotdave <islandmonkeee@gmail.com>
2023-12-23 10:08:37 +0000

    different data in enums

--- src/enums.rs
+++ src/enums.rs
@@ -19,3 +19,17 @@ fn cast_enums_as_ints() -> () {
     // This shows how to use enums as integers by casting them with the 'as' keyword.
     assert_ne!(Number::One as u8, Number::Two as u8);
 }
+
+// This shows how enums can hold different types of data.
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32)
+}
+
+// This shows how to instantiate those different data types.
+fn message_impl() -> () {
+    let msg1 = Message::Move { x: (3), y: (4) };
+    let msg2 = Message::Write(String::from("hello world"));
+}
