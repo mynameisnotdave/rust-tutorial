cf50482acbf23e5fc9e25808850694cf00ae3b8e
mynameisnotdave <islandmonkeee@gmail.com>
2023-12-28 17:45:48 +0000

    static and dynamic dispatch differences

--- src/associated_types.rs
+++ src/associated_types.rs
@@ -1,5 +1,3 @@
-use std::fmt::format;
-
 trait MyTrait {
     // Inside of traits we can define associated types.
     type MyType;
@@ -33,4 +31,27 @@ impl Foo for String {
     fn method(&self) -> String {
         format!("string: {}", *self)
     }
+}
+
+// Static dispatch is done by implementing the expected types that the function will take.
+// The trait's methods can then work properly.
+fn static_dispatch<T: Foo>(a: T) {
+    a.method();
+}
+
+fn static_dispatch_u8<T: Foo>(a: u8) {
+    a.method();
+}
+
+// Dynamic dispatch is done by the dyn type which will find out which impl to call at
+// runtime.
+fn dynamic_dispatch(a: &dyn Foo) {
+    a.method();
+}
+
+fn misc() -> () {
+    let x: u8 = 5;
+    let y: String = "Hello".to_string();
+    static_dispatch(x);
+    dynamic_dispatch(&y);
 }
