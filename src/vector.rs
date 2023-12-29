b65b5fe5c3e7c7597308134953ce3f85e7d88187
mynameisnotdave <islandmonkeee@gmail.com>
2023-12-29 18:31:10 +0000

    array to vector, pop and push vec, extend

---
+++ src/vector.rs
@@ -0,0 +1,42 @@
+fn vecs() -> () {
+    let arr: [u8; 3] = [1, 2, 3];
+    // Use Vec::from() to convert an array into a vector.
+    let v: Vec<u8> = Vec::from(arr);
+
+    // vec![] and vec!() are the same thing
+    let v: Vec<u8> = vec![1, 2, 3];
+    is_vec(v);
+
+    let v: Vec<u8> = vec!(1, 2, 3);
+    is_vec(v.clone());
+
+    // Using vec! with an array will create a halfway sort of type that is Vec<[u8; 3]>,
+    // where the Vec's type is of an array.
+    let mut v1: Vec<u8> = Vec::new();
+    is_vec(v1.clone());
+
+    for i in &v {
+        v1.push(*i);
+    }
+
+    assert_eq!(v, v1);
+
+    println!("Success!");
+}
+
+fn is_vec(v: Vec<u8>) {}
+
+fn extend_vector() -> () {
+    let mut v1: Vec<i32> = Vec::from([1, 2, 4]);
+    v1.pop(); // This removes the last element (4)
+    v1.push(3); // This adds 3 to end of the vector
+
+    // This is how to 'map' a new vector from an old one, by using the extend function.
+    // The extend function's parameter should be a borrowed version of the old vector.
+    let mut v2: Vec<i32> = Vec::new();
+    v2.extend(&v1);
+
+    assert_eq!(v1, v2);
+
+    println!("Success!");
+}
