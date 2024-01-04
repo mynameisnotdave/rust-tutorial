f235c279c1585949c7a5ae43c2573de92e2426d7
mynameisnotdave <islandmonkeee@gmail.com>
2024-01-04 21:14:04 +0000

    allow overflow

--- src/typecoercion.rs
+++ src/typecoercion.rs
@@ -1,7 +1,7 @@
 /* The video explains that 1000 as u8 would equal a value of 232. This is because u8 accepts
     a maximum value of 256, and type coercion in Rust means that the compiler will force the
-    defined value to adhere to the rules of the type that it is forced into. So 1000 - 256
-    = 744, still too big. 744-256=488, still too big. 488-256 = 232, this is okay as u8.
+    defined value to adhere to the rules of the type that it is forced into, IF the allow overflow attribute is defined. 
+    So 1000 - 256 = 744, still too big. 744-256=488, still too big. 488-256 = 232, this is okay as u8.
 
     -1i8 as u8 would make -1 = 255, as it will turn the value on its head.
 */
@@ -17,4 +17,9 @@ fn coercion() -> () {
     assert_eq!(integer + 1, 'b' as u8);
 
     println!("Success!");
+}
+
+#[allow(overflowing_literals)] // required to allow overflow
+fn allow_overflow() -> () {
+    let v: u8 = 1000 as u8;
 }
