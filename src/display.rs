822aad423c66b4611b032f137eb3d31358664929
mynameisnotdave <islandmonkeee@gmail.com>
2024-01-27 13:36:45 +0000

    display module

--- src/display.rs
+++ src/display.rs
@@ -1,1 +1,24 @@
 use std::fmt;
+
+struct Point {
+    x: i32,
+    y: i32
+}
+
+// This is how one can manually derive the Display trait for their custom type to
+// print the type's properties out.
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // No semicolon here as this function is not void.
+        write!(f, "The point is ({}, {})", self.x, self.y)
+    }
+}
+
+fn point_to_string() -> () {
+    let origin: Point = Point { x: 0, y: 0 };
+
+    assert_eq!(origin.to_string(), "The point is (0,0)");
+    assert_eq!(format!("{}", origin), "The point is (0,0)");
+
+    println!("Success!");
+}
