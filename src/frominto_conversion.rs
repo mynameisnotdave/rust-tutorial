6524d050582258bb4f98d9c9e4652d905caa8e0a
mynameisnotdave <islandmonkeee@gmail.com>
2024-01-27 13:24:12 +0000

    more try from stuff

--- src/frominto_conversion.rs
+++ src/frominto_conversion.rs
@@ -1,3 +1,4 @@
+use std::fmt::Result;
 use std::fs;
 use std::io;
 use std::num;
@@ -51,7 +52,58 @@ impl From<num::ParseIntError> for CliError {
 }
 
 fn open_and_parse_file(file_name: &str) -> Result<i32, CliError> {
+    // The question marks below is to associate that result with a particular error.
+    // In this case, the contents variable will be associated with the IoError, 
+    // and the num variable will be associated with the ParseError.
     let contents: String = fs::read_to_string(&file_name)?;
     let num: i32 = contents.trim().parse()?;
     return Ok(num)
+}
+
+fn try_from_into() -> () {
+    let n: i16 = 256;
+
+    // This is a way to catch type conversions that may fail.
+    // Instead of confidently using the 'into' method, we have a 'try into' which will
+    // provide a way for us to error check the conversion gracefully.
+    // In this case, this conversion will not work. It will throw an error as 256 is
+    // too big for i16.
+    let n: u8 = match n.try_into() {
+        Ok(n) => n,
+        Err(e) => {
+            println!("caught error trying to convert {:?}", e.to_string());
+            0
+        }
+    };
+    assert_eq!(n, 0);
+    println!("Success!")
+}
+
+#[derive(Debug, PartialEq)]
+struct EvenNum(i32);
+
+// This is a way to test for even numbers using the try_from function.
+impl TryFrom<i32> for EvenNum {
+    type Error = ();
+
+    fn try_from(value: i32) -> Result {
+        if value % 2 == 0 {
+            Ok(EvenNum(value))
+        }
+        else {
+            Err(())
+       }
+    }
+}
+
+fn even_num() -> () {
+    assert_eq!(EvenNum::try_from(8), Ok(EvenNum(8)));
+    assert_eq!(EvenNum::try_from(5), Err(()));
+
+    let result: Result<EvenNum, ()> = 8i32.try_into();
+    assert_eq!(result, Ok(EvenNum(8)));
+    let result: Result<EvenNum, ()> = 5i32.try_into();
+    assert_eq!(result, Err(()));
+
+    println!("Success!");
 }
