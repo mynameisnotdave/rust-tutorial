fe9632dacc4d65f06fdd1c01a1cd7fda9d9e0ab6
Aidan Fell <islandmonkeee@gmail.com>
2024-02-04 13:32:27 +0000

    do the divide and parse int error

--- src/result.rs
+++ src/result.rs
@@ -1,3 +1,5 @@
+use std::num::ParseIntError;
+
 // Error handling. The second value of the generic is a string because the error returns
 // a string message.
 fn divide(x: f32, y: f32) -> Result<f32, &'static str> {
@@ -5,4 +7,24 @@ fn divide(x: f32, y: f32) -> Result<f32, &'static str> {
         return Err("Division by zero");
     }
     Ok(x / y)
+}
+
+fn do_the_divide() -> () {
+    let result = divide(10.0, 2.0);
+
+    // This does something with the result based upon the arguments given inside the
+    // result-bound function.
+    match result {
+        Ok(val) => println!("Result, {}", val),
+        Err(msg) => println!("Error: {}", msg),
+    }
+}
+
+fn something_or_parseinterror() -> Result<(), ParseIntError> {
+    let number_str = "10";
+    // the ? is a shorthand match for any result functions.
+    // it tells the compiler that this variable might be associated with an error result.
+    let number = number_str.parse::<i32>()?; 
+    println!("{}", number);
+    Ok(())
 }
